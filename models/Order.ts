799a53a5e0c3e5f90b3732b1456f51b43d110f3c
travis <trav.horn@gmail.com>
2022-09-01 20:28:46 -0600

    Order ts conversion

---
+++ models/Order.ts
@@ -0,0 +1,59 @@
+import { Document, Model, models, model, Schema, Types } from 'mongoose';
+
+interface IOrder extends Document {
+	user: Types.ObjectId;
+	storeId: Types.ObjectId;
+	rentals: Array<Types.ObjectId>;
+	paymentMethod: String;
+	paymentResult?: {
+		id: String;
+		status: String;
+		email_address: String;
+	};
+	itemsPrice: Number;
+	taxPrice: Number;
+	totalPrice: Number;
+	isPaid: Boolean;
+	paidAt?: Date;
+}
+
+const orderSchema: Schema = new Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		storeId: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		rentals: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Rental',
+			},
+		],
+		paymentMethod: { type: String, required: true },
+		paymentResult: { id: String, status: String, email_address: String },
+		itemsPrice: { type: Number, required: true },
+		taxPrice: { type: Number, required: true },
+		totalPrice: { type: Number, required: true },
+		isPaid: { type: Boolean, required: true, default: false },
+		paidAt: { type: Date },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+/**
+ * Methods
+ */
+
+/**
+ * Statics
+ */
+
+export default models['Order'] || model<IOrder>('Order', orderSchema);
